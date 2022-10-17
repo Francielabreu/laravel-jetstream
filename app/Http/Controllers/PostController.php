ed5a3a6f391f4c2522a01b7d05e1dbb8b7e98f4a
= <=>
2022-10-17 11:11:48 -0300

    feat : Implemented listing method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+    public function index()
+    {
+        $posts = $this->post->get();
+
+        return view('posts.index', compact('posts'));
+    }
 }
